de490c3a5ea5efc70ed099f08360c07461a6a676
galehouse5 <galehouse.5@gmail.com>
2017-12-18 00:16:12 -0500

    Initial commit.

---
+++ FastiCalSync.Tests/Registry.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FastiCalSync.Tests
+{
+    public static class Registry
+    {
+        private static IConfigurationRoot appSettings;
+        public static IConfigurationRoot AppSettings
+        {
+            get
+            {
+                if (appSettings == null)
+                {
+                    IConfigurationBuilder builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    appSettings = builder.Build();
+                }
+
+                return appSettings;
+            }
+        }
+    }
+}
